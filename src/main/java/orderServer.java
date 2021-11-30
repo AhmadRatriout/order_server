13a95177be5175c6508742da75c70b29adc1e21f
Mahdi Barham <mahdi-99@live.com>
2021-11-30 18:48:14 +0200

    inline-Documentation

--- src/main/java/orderServer.java
+++ src/main/java/orderServer.java
@@ -8,12 +8,12 @@ import static spark.Spark.*;
 public class orderServer {
     public static void main(String[] args) {
         BasicConfigurator.configure();
-
+        //this is the purchase request recieved by the order server from the front-end-server
         post("/purchase/:value", (request, response) -> {
 
             String value = request.params(":value");
-            System.out.println("HHHHHHHHHHHHH "+value+" hhhhhhhhhh");
-            
+           
+            //here we take the value from the request URL and add it to the URL in the request to be send to the catalog server
             
             URL url = new URL("http://192.168.1.250:4567/purchase/"+value);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
